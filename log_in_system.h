e8c938eb502f0d14f49737f215980c4dae9c47b8
Mariam Esmat <100226656+mariamessmatt@users.noreply.github.com>
2023-03-31 19:01:44 +0200

    Create log_in_system.h

---
+++ log_in_system.h
@@ -0,0 +1,68 @@
+#ifndef LOG_IN_SYSTEM_H_INCLUDED
+#define LOG_IN_SYSTEM_H_INCLUDED
+
+//log_in_system.h
+//
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <regex>
+
+
+using namespace std;
+
+int ctr=0;
+fstream my_file;
+
+string file_name = "users.txt" ;
+
+string text;
+
+stringstream line;
+
+
+typedef struct
+{
+    string id ;
+    string Email;
+    string Phone;
+    string pasword;
+
+}user;
+
+
+void check_operation(string& number);
+
+
+void get_data();
+
+void put_data();
+
+bool strongPass(string pass);
+
+string coverPassword();
+
+bool valid_UserName(string& username);
+
+bool valid_email(string& email);
+
+bool valid_phone(string& phone);
+
+istream &operator>> (stringstream &in, user& x  );
+
+string PassWord();
+
+void UserData();
+
+void login();
+
+void change_password();
+
+
+
+
+#endif // LOG_IN_SYSTEM_H_INCLUDED
